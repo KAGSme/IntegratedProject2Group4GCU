248f8f39382f3f998de47cda8b91628ec7f95375
ricardobarros95 <ricardobarros95@gmail.com>
2015-02-07 13:24:02 +0000

    Test scene, player and PowerBalance script

---
+++ IP2 Game Project/Assets/scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Player : MonoBehaviour {
+
+    private float playerScore;
+    public GameObject[] player1Particles = new GameObject[9];
+    public GameObject[] player2Particles = new GameObject[9];
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SwipePower();
+	}
+
+    //when you get the input, it creates a ray at mouse position, then it's going to check if the object hit was one of player 1 particles or one of player 2 particles.
+    //Then its going to get the PowerBalance script of the hit object to increase its energy level
+    //And then its going to get the PowerBalance script of the mirrored object and decrease its value;
+    //The way this works is: In the Unity Editor, all particles are named from 1-9, when you hit a particle (e.g. 3) and if it belongs to the player1Particles[],
+    //its going to look in the player2Particles[] for the same name. 
+    void SwipePower()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.tag == "player1particles") 
+                {
+                    PowerBalance power1 = hit.transform.GetComponent<PowerBalance>();
+                    power1.IncreaseValue();
+                    int i = Convert.ToInt32(hit.transform.name); 
+                    PowerBalance power2 = player2Particles[i-1].GetComponent<PowerBalance>(); //i-1 because array starts at 0
+                    power2.DecraseValue();
+                    
+                }
+                else if (hit.collider.gameObject.tag == "player2particles")
+                {
+                    PowerBalance power1 = hit.transform.GetComponent<PowerBalance>();
+                    power1.IncreaseValue();
+                    int i = Convert.ToInt32(hit.transform.name);
+                    PowerBalance power2 = player1Particles[i-1].GetComponent<PowerBalance>();
+                    power2.DecraseValue();
+                }
+
+            }
+        }
+    }
+
+    float TotalScore()
+    {
+        return playerScore;
+    }
+}
---
+++ IP2 Game Project/Assets/scripts/PowerBalance.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerBalance : MonoBehaviour {
+
+    public float particleValue = 50; //The energy level they start with
+    public float valueFactor = 1; //How much they increase/decrease per touch
+    
+	
+    //Use this method when you want to increase the energy level
+    public void IncreaseValue()
+    {
+        particleValue += valueFactor;
+    }
+
+    //And this one to decrease it. 
+    public void DecraseValue( )
+    {
+        particleValue -= valueFactor;        
+    }
+    
+    //Going to be used by the Player script to return the total score of the player, by summing up all particle values
+    float GetParticleValue()
+    {
+        return particleValue;
+    }
+
+}
