8e0b442fe959329ded8985b17229fc4d0492a2f7
Kieran <kags1995@hotmail.co.uk>
2015-02-24 09:34:15 +0000

    Quick fixes

---
+++ IP2 Game Project/Assets/scripts/NetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private int port = 25000;
+    private int maximumPlayerAmount = 2;
+    private const string  typeName = "RubIt_Multiplayer_Balance_of_Power";
+    private const string gameName = "Room_Name";
+
+    private void StartServer()
+    {
+        Network.InitializeServer(maximumPlayerAmount, port, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(typeName, gameName);
+    }
+
+    void OnServerInitialized()
+    {
+        Debug.Log("Server Initialized");
+    }
+}
