daa2b22e3b862bf7b263734f5486d5d29d4bc63a
ricardobarros95 <ricardobarros95@gmail.com>
2015-03-08 23:02:29 +0000

    more fixes and more UI stuff

--- IP2 Game Project/Assets/scripts/GameControl_PowerBalanceMode.cs
+++ IP2 Game Project/Assets/scripts/GameControl_PowerBalanceMode.cs
@@ -11,49 +11,42 @@ public class GameControl_PowerBalanceMode : MonoBehaviour {
     public State state;
     public Player[] player = new Player[2];
     public Timer1 timer;
-    private string winnerMessage;
+    public string winnerMessage;
     public float winningScore;
     public GameObject playAgainButton;
     public GameObject QuitButton;
     private Image powerBar;
     public float totalScore;
     public GameObject powerObject;
+    public GameObject powerBarObject;
+    Text winnerText;
+    public GameObject winnerTextGameObject;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
         state = State.Start;
         Time.timeScale = 1;
         powerBar = powerObject.GetComponent<Image>();
+        winnerText = winnerTextGameObject.GetComponent<Text>();
+
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         DecideWinner();
         ButtonState();
-        totalScore = player[0].playerScore + player[1].playerScore;
-        if (player[0].playerScore == player[1].playerScore)
+        PowerBar();
+        if (state == State.End)
         {
-            powerBar.fillAmount = 0.5f;
+            winnerTextGameObject.active = true;
         }
         else
         {
-            powerBar.fillAmount = player[0].playerScore / totalScore;
-
+            winnerTextGameObject.active = false;
         }
-
-	}
-
-
-    void OnGUI()
-    {
-
-        if (state == State.End)
-        {
-            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 800, 400), winnerMessage + Convert.ToInt32(winningScore) + " points");
-
-        }
-        
     }
     void ButtonState()
     {
@@ -74,23 +67,48 @@ public class GameControl_PowerBalanceMode : MonoBehaviour {
         Application.Quit();
     }
 
+    void PowerBar()
+    {
+        if (state == State.Running)
+        {
+            powerBarObject.active = true;
+        }
+        else
+        {
+            powerBarObject.active = false;
+        }
+
+        totalScore = player[0].playerScore + player[1].playerScore;
+        if (player[0].playerScore == player[1].playerScore)
+        {
+            powerBar.fillAmount = 0.5f;
+        }
+        else
+        {
+            powerBar.fillAmount = player[0].playerScore / totalScore;
+
+        }
+    }
+
     void DecideWinner()
     {
-        if (timer.timer == 0)
+        if (state == State.End)
         {
             if (player[0].PlayerEnergyTracker() > player[1].PlayerEnergyTracker())
             {
-                state = State.End;
+                //state = State.End;
                 Time.timeScale = 0;
                 winningScore = player[0].PlayerScore;
-                winnerMessage = "Player 1 wins with ";
+                winnerMessage = "Blue wins with " + Convert.ToString(winningScore);
+                winnerText.text = winnerMessage;
             }
             else
             {
-                state = State.End;
+               // state = State.End;
                 Time.timeScale = 0;
                 winningScore = player[1].PlayerScore;
-                winnerMessage = "Player 2 wins with ";
+                winnerMessage = "Red wins with " + Convert.ToString(winningScore);
+                winnerText.text = winnerMessage;
             }
         }
 
