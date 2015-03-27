85f770cf1ee4acdbb951e957c48cf14000324693
KAGSme <kags1995@hotmail.co.uk>
2015-03-27 14:14:33 +0000

    powerbase

--- IP2 Game Project/Assets/scripts/PowerUp_Base.cs
+++ IP2 Game Project/Assets/scripts/PowerUp_Base.cs
@@ -3,68 +3,25 @@ using System.Collections;
 
 public class PowerUp_Base : MonoBehaviour {
 
-    public float coolDown = 10f;
-    public float planetEnergyActivation = 15.0f;
-    private bool isActive;
-    public float stationaryTouchSpeed = 0.5f;
-    public Planet planet;
-    public GameObject planetObject;
+    [Range(0,500)]
+    public int maxScoreLimit = 400;
+    [Range(0,500)]
+    public int minScoreLimit = 50;
+    Player player;
+    public bool isActive;
 
-    public bool IsActive
+    void Start()
     {
-        get { return isActive; }
-        set { isActive = value; }
-    }
-
-	// Use this for initialization
-	void Start () {
         isActive = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        CoolDownCheck();
-        PowerActivation();
-	}
-    
-    float lastTimeCheck = 0f;
-    void CoolDownCheck()
-    {
-        float timer = Time.timeSinceLevelLoad;
-        if (timer - lastTimeCheck >= coolDown)
-        {
-            isActive = false;
-            lastTimeCheck = timer;
-        }
     }
 
-    void PowerActivation()
+    void OnEnable()
     {
-        if (planet.energy <= planetEnergyActivation && IsActive == false)
-        {
-            if (Input.touchCount > 0)
-            {
-                for (int i = 0; i < Input.touchCount; i++)
-                {
-                    Touch touch = Input.GetTouch(i);
-                    RaycastHit hit;
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    Debug.Log("touchDetected");
-                    
-                    if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
-                    {
-                        if (Physics.Raycast(ray, out hit))
-                        {
-                            if (hit.collider.gameObject == this && touch.deltaPosition.magnitude <= stationaryTouchSpeed)
-                            {
-                                IsActive = true;
-                                CoolDownCheck();
-                            }
-                        }
-                    }
-                }
-            }
-        }
-                                
+        GameControl_PowerBalanceMode.gameControl.gameRun += EnergyCheck;
+    }
+
+    void EnergyCheck()
+    {
+
     }
 }
