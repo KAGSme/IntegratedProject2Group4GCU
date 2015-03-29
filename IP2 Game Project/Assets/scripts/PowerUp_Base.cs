77ece29faf5044a255f2c2f7d764ad118ee435ea
Kieran <kags1995@hotmail.co.uk>
2015-03-29 14:29:48 +0100

    script update

--- IP2 Game Project/Assets/scripts/PowerUp_Base.cs
+++ IP2 Game Project/Assets/scripts/PowerUp_Base.cs
@@ -3,16 +3,28 @@ using System.Collections;
 
 public class PowerUp_Base : MonoBehaviour {
 
+    public delegate void PowerUpFunc();
+    public PowerUpFunc PowerUp;
+
     [Range(0,500)]
     public int maxScoreLimit = 400;
     [Range(0,500)]
     public int minScoreLimit = 50;
-    Player player;
-    public bool isActive;
+    public Player player;
+    bool isActive;
+    PowerUp_Freeze freeze;
+
+
+    public bool IsActive
+    {
+        get { return isActive; }
+        set { isActive = value; }
+    }
 
     void Start()
     {
         isActive = false;
+        freeze = GetComponent<PowerUp_Freeze>();
     }
 
     void OnEnable()
@@ -22,6 +34,21 @@ public class PowerUp_Base : MonoBehaviour {
 
     void EnergyCheck()
     {
+            if (!isActive)
+        {
+            if (player.PlayerScore >= maxScoreLimit || player.PlayerScore <= minScoreLimit)
+            {
+                IsActive = true;
+                if (PowerUp != null)
+                {
+                    PowerUp();
+                }
+            }
+        }
+    }
 
+    public void SetFreeze()
+    {
+        PowerUp += freeze.Freeze;
     }
 }
--- IP2 Game Project/Assets/scripts/PowerUp_Freeze.cs
+++ IP2 Game Project/Assets/scripts/PowerUp_Freeze.cs
@@ -4,39 +4,16 @@ using System.Collections;
 public class PowerUp_Freeze : MonoBehaviour {
 
     public Player opposingPlayer;
-    public PowerUp_Base powerUpBase;
     public float freezeTime = 3f;
+    public GameObject FreezeParticles;
 
-	// Use this for initialization
-	void Awake () {
-        if (powerUpBase.planet.belongsToPlayer == PlayerNumber.player1)
-        {
-            opposingPlayer = GameObject.Find("Player 2").GetComponent<Player>();
-        }
-        else if (powerUpBase.planet.belongsToPlayer == PlayerNumber.player2)
-        {
-            opposingPlayer = GameObject.Find("Player 1").GetComponent<Player>();
-        }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (powerUpBase.IsActive == true)
-        {
-            Freeze(freezeTime);
-        }
-	}
-
-    void Freeze(float waitTime)
+    public void Freeze()
     {
-        if (powerUpBase.IsActive == true)
+        float timer = Time.time;
+        opposingPlayer.IsActive = false;
+        if (Time.time - timer == freezeTime)
         {
-            float timer = Time.time;
-            opposingPlayer.IsActive = false;
-            if (Time.time - timer == waitTime)
-            {
-                opposingPlayer.IsActive = true;
-            }
+            opposingPlayer.IsActive = true;
         }
     }
 }
